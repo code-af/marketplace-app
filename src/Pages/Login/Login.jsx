1ccda24ea039ec339924a1b7d3b57fdc854708e9
code-af <afsalmanzil445@gmail.com>
2026-09-01 17:54:49 +0530

    finished setting the Login component for login page includes a form and also a function for authentication with firebase  using auth object

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,5 +1,48 @@
-import React from "react";
+import logo from '../../assets/ebuy-icon.png'
+import React,{useState} from "react";
+import {signInWithEmailAndPassword} from 'firebase/auth'
+import {auth} from '../../firebase/config'
+import { useNavigate } from 'react-router-dom'
+import './Login.css'
+
 function Login(){
-    return <h1>Login</h1>
+    const navigate = useNavigate()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+    const [successMsg, setSuccessMsg] = useState('')
+
+    const handleLogin = (e)=>{
+        e.preventDefault()
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential)=>{
+            console.log(userCredential.user)
+            navigate('/')
+        }).catch((error)=>{
+            setError(error.message)
+        })
+    }
+    return <>
+        <div className="login-header">
+            <a href="/home"><img src={logo} alt="logo" /></a>
+        </div>
+        <div className="login-container">
+            <div className="login-form">
+                <h1>Sign in to your account</h1>
+                <h4>Are you new here ?</h4>
+                <button className='create-btn' onClick={()=>navigate('/signup')}>Create account</button>
+                <form onSubmit={handleLogin}>
+                    <input type="email" value={email} placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+                    <input type="password" value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                    {successMsg && <p className="success-msg">{successMsg}</p>}
+                    {error && <p className="error-msg">{error}</p>}
+                    <button className='submit-btn' type='submit'>Continue</button>
+                </form>
+            </div>
+        </div>
+        <div className="login-footer">
+            <p>Copyright © 1995-2026 eBuy Inc. All Rights Reserved. </p>
+        </div>
+    </>
 }
 export default Login
