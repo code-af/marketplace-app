4ab0118db812d200526badf8c30b0c2bc96417c1
code-af <afsalmanzil445@gmail.com>
2026-09-08 21:58:32 +0530

    Finished styling and structuring the footer component for the web app, added footer links, a navigation to top button, styled with css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, useLocation } from 'react-router-dom'
 import Header from './Components/Header/Header'
-import Footer from `./Components/Footer/Footer`
+import Footer from './Components/Footer/Footer'
 import Home from './Pages/Home/Home'
 import Login from './Pages/Login/Login'
 import Signup from './Pages/Signup/Signup'
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,35 @@
 import React from "react";
+import './Footer.css'
 function Footer(){
-    return <h2>Footer</h2>
+    return (<>
+        <footer className="footer">
+            <div className="footer-top">
+                <a href="#">About</a>
+                <a href="#">Announcements</a>
+                <a href="#">Community</a>
+                <a href="#">Security</a>
+                <a href="#">Seller Center</a>
+                <a href="#">Policies</a>
+                <a href="#">Affiliates</a>
+                <a href="#">Product Safety Tips</a>
+                <a href="#">Help & Contact</a>
+                <a href="#">Site Map</a>
+            </div>
+            <a href="#" title="Scroll to Top"><button className="scroll-to-top-btn">⮝</button></a>
+            <div className="footer-bottom">
+                <p className="footer-copy">Copyright © 2026 eBuy. All rights reserved.
+                    <a href="#">Accessibility</a>
+                    <a href="#">User Agreement</a>
+                    <a href="#">Privacy</a>
+                    <a href="#">Consumer Health Data</a>
+                    <a href="#">Payment Services</a>
+                    <a href="#">Cookies</a>
+                    <a href="#">Privacy Notice</a>
+                    <a href="#">Privacy Choices</a> and 
+                    <a href="#">Adchoice 🛈</a>
+                </p>
+            </div>
+        </footer>
+    </>)
 }
 export default Footer
