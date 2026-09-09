30b9e537a5b80ee9d12a8e413eb05f6216f9c8dd
code-af <afsalmanzil445@gmail.com>
2026-09-09 14:53:32 +0530

    Finished the setup of Product page Component using axios, for fetching fakestore API then using state for storing products finally iterating the product component using map method, while passing props for the title and image of product

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import ProductCard from '../../Components/ProductCard/ProductCard'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useSearchParams } from 'react-router-dom'
 function Products(){
-    return <h1>Products</h1>
+    const [products, setProducts] = useState([])
+    const [searchParams] = useSearchParams()
+    const category = searchParams.get('category')
+
+    useEffect(() => {
+        const url = category
+            ? `https://fakestoreapi.com/products/category/${category}`
+            : `https://fakestoreapi.com/products`
+
+        axios.get(url)
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+    }, [category])
+    return (
+        <>
+        {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+        ))}
+        </>
+    )
 }
 export default Products
