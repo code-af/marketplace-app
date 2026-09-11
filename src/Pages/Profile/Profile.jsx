f84fe9061b5fc45cb40d312b24601907c55de3a2
code-af <afsalmanzil445@gmail.com>
2026-09-11 18:04:18 +0530

    initiated the profile page setup for the user account that tracks the current user data, selling item listings and logout authentication feature along with authentication context

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,5 +1,18 @@
-import React from "react";
+import { useState, useEffect } from 'react'
+import { doc, getDoc, collection, query, where, getDocs, deleteDoc } from 'firebase/firestore'
+import { db, auth } from '../../firebase/config'
+import { useAuth } from '../../context/Authcontext'
+import { useNavigate } from 'react-router-dom'
+import { signOut } from 'firebase/auth'
+import './Profile.css'
 function Profile(){
+    const [activeTab, setActiveTab] = useState('listings')
+    const [userListings, setUserListings] = useState([])
+    const [userData, setUserData] = useState(null)
+    const { currentUser } = useAuth()
+    const navigate = useNavigate()
+
+    
     return <h1>Profile</h1>
 }
 export default Profile
