dc5dd5b4edc299aca996183749f9b144f7da99d7
code-af <afsalmanzil445@gmail.com>
2026-09-15 14:44:29 +0530

    Imported the modules needed for the sell form page, declared some hooks for managing state, useeffect , navigate and auth, used useeffect for checking if the current user is logged in, if not redirect to login page

--- src/Pages/Sell/Sell.jsx
+++ src/Pages/Sell/Sell.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import { useState, useEffect } from 'react'
+import { collection, addDoc } from 'firebase/firestore'
+import { db } from '../../firebase/config'
+import { useAuth } from '../../context/Authcontext'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import './Sell.css'
 function Sell(){
+    const [title, setTitle] = useState('')
+    const [price, setPrice] = useState('')
+    const [category, setCategory] = useState('')
+    const [description, setDescription] = useState('')
+    const [imageUrl, setImageUrl] = useState('')
+    const [categories, setCategories] = useState([])
+    const [successMsg, setSuccessMsg] = useState('')
+    const [error, setError] = useState('')
+    const [userData, setUserData] = useState(null)
+    const { currentUser } = useAuth()
+    const navigate = useNavigate()
+
+    useEffect(() => {  // redirect if current user isn't logged in
+        if(!currentUser) navigate('/login')
+    }, [currentUser])
+    
     return <h1>Sell</h1>
 }
 export default Sell
